fec34e2b2c41e884a5ddbfdc2000f1d9ecc05568
DanielPawlowicz <danielpawlowicz@interia.pl>
2023-11-16 13:25:13 +0100

    testing if js works as i remember

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // console.log("haha0");
+
+/*
+
+1. Layout and single task (html + css)
+2. Creating a new 
+
+
+*/
+
+const add_new_task = () => {
+    const task = prompt("Name the task");
+    // Creating new task as an object 
+}
