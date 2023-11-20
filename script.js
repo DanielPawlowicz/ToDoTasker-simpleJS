fc4dc577a1a6eb301fb61af884fa408fc1309dd3
DanielPawlowicz <danielpawlowicz@interia.pl>
2023-11-20 20:58:16 +0100

    stiling css and so on

--- script.js
+++ script.js
@@ -2,13 +2,26 @@
 
 /*
 
-1. Layout and single task (html + css)
-2. Creating a new 
+1. Layout and single task stiling (html + css)
+2. Creating a new task
+3. Creating sub-tasks
+4. Managing task (deleting, changing order, changing text)
 
 
 */
 
+const Task = {
+
+    showTask : function () {
+        console.log("pojawilo się zadanko")
+    }
+};
+
 const add_new_task = () => {
-    const task = prompt("Name the task");
+    const textOfTask = prompt("Name the task");
+    const task1 = Object.create(Task);
+    Task.value = textOfTask;
+    task1.showTask();
+    //console.log(task1.value);
     // Creating new task as an object 
 }
